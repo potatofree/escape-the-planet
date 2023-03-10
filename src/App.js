a586e3cc755bb0a718cc58c7e6eebd214a7daaed
Aliaksandr Volkau <58636552+potatofree@users.noreply.github.com>
2023-03-10 18:15:39 +0100

    image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import image from'./image.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="../public/image.jpg" alt="Planet"/>
+        <img src={image} alt="Planet"/>
         <p>
           There will be something
         </p>
