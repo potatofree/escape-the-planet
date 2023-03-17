c9a22fe7f099bfcb75f764abf4eac49cfdc58d2e
Aliaksandr Volkau <58636552+potatofree@users.noreply.github.com>
2023-03-17 17:21:36 +0100

    Gathering resources

--- src/gameLog.js
+++ src/gameLog.js
@@ -1,7 +1,11 @@
 export const logs = [];
 export const gameLog = (step, ...arr) => {
   if (step === "materials") {
-    logs.unshift(`You have get ${arr[0]} materials`);
+    logs.unshift(`You've got ${arr[0]} materials`);
+  } else if (step === "resources") {
+    logs.unshift(
+      `You've got ${arr[0].map((res, i) => "res_" + i + ":" + res).join(", ")}`
+    );
   } else {
     logs.unshift(`${step} happend with ${arr} arguments`);
   }
