657aaf2f75f48d7d9db3fbdcae848633c7a7861f
Aliaksandr Volkau <58636552+potatofree@users.noreply.github.com>
2023-03-22 17:55:39 +0100

    Counter added

--- src/Game.js
+++ src/Game.js
@@ -5,13 +5,17 @@ import { logs, gameLog } from "./gameLog";
 export const Game = (props) => {
   const [materials, setMaterials] = useState(0);
   const [resources, setResources] = useState([0, 0, 0]);
+  const [counter, setCounter] = useState(0);
+  const counterInc = () => setCounter(counter + 1);
   const handleMaterials = () => {
+    counterInc();
     const materialsInc = [0, 1, 1, 2, 2, 3];
     const materialsRand = Math.floor(materialsInc.length * Math.random());
     setMaterials((materials) => materials + materialsInc[materialsRand]);
     gameLog("materials", materialsInc[materialsRand]);
   };
   const handleResources = () => {
+    counterInc();
     const resInc = [0, 1, 1, 1, 2, 2, 2, 3, 3, 4];
     const resRand = () => Math.floor(resInc.length * Math.random());
     const resAdd = [0, 0, 0].map((e) => resInc[resRand()]);
@@ -25,6 +29,7 @@ export const Game = (props) => {
       <>
         <div>
           <p>Game status: Started</p>
+          <p>Days: {counter}</p>
           <Button onClick={handleMaterials}>Harvest Materials</Button>
           <Button onClick={handleResources}>Gather Resources</Button>
           <p>Materials: {materials}</p>
