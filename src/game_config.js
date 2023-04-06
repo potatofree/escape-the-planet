50c613488156806e2f18d7970a36299b56250768
Aliaksandr Volkau <58636552+potatofree@users.noreply.github.com>
2023-04-06 16:52:27 +0200

    resource limit

---
+++ src/game_config.js
@@ -0,0 +1,3 @@
+export const exploringResourcesLimit = { Air: 20, Food: 0, Energy: 20 }; // minimum number of resources to start exploring
+export const materialsGatheringIncrement = [0, 1, 1, 2, 2, 3]; // distribution of increments with equal probability
+export const resourcesGainingIncrement = [0, 1, 1, 1, 2, 2, 2, 3, 3, 4]; // distribution of increments with equal probability
